4c5f49100432c9a58afed5433171f5a68b229712
SamCH93 <samuel.pawel@gmail.com>
2025-10-22 17:32:48 +0200

    add test for pbf01seq

--- package/inst/tinytest/test-pbf01seq.R
+++ package/inst/tinytest/test-pbf01seq.R
@@ -1,57 +1,100 @@
-## ## do not run these tests for the moment, because they are there to verify
-## ## the power with simulation which takes a long time to run
-
-## set.seed(42)
 ## library(tinytest)
 ## library(bfpwr)
 
-## ## compare to simulation-based probabilities
-## n <- seq(10, 50, 5) # sample size per stage
-## se <- sqrt(2/n) # standard errors per stage
-## dpm <- 0.3
-## dpsd <- 0.1
-## pm <- 0.3
-## psd <- 0.5
-## type <- "normal"
-## k0 <- 6
-## k1 <- 1/30
-## nsim <- 100000
-## results <- replicate(n = nsim, expr = {
-##     smd <- rnorm(n = 1, mean = dpm, sd = dpsd)
-##     y1 <- rnorm(n = max(n), mean = 0, sd = 1)
-##     y2 <- rnorm(n = max(n), mean = smd, sd = 1)
-##     smd <- sapply(seq_along(n), FUN = function(i) {
-##         (mean(y2[1:n[i]]) - mean(y1[1:n[i]]))
+## ## check with simulation that correct probabilities calculated
+## simbenchmark <- function(nsim, k1, k0, usd, n, pm, psd, dpm, dpsd, type) {
+##     ## simulate stage-wise BFs
+##     smd <- rnorm(n = nsim, mean = dpm, sd = dpsd)
+##     bfmat <- sapply(X = smd, FUN = function(smdi) {
+##         y1 <- rnorm(n = max(n), mean = 0, sd = usd)
+##         y2 <- rnorm(n = max(n), mean = smdi, sd = usd)
+##         est <- sapply(seq_along(n), FUN = function(i) {
+##             (mean(y2[1:n[i]]) - mean(y1[1:n[i]]))/usd
+##         })
+##         se <- sapply(seq_along(n), FUN = function(i) {
+##             sqrt(2/n[i])
+##         })
+##         bf <- sapply(seq_along(n), FUN = function(i) {
+##             if (type == "normal") {
+##                 bf01(estimate = est[i], se = se[i], null = 0, pm = pm,
+##                      psd = psd)
+##             } else if (type == "directional") {
+##                 dirbf01(estimate = est[i], se = se[i], null = 0, pm = pm,
+##                         psd = psd)
+##             } else {
+##                 nmbf01(estimate = est[i], se = se[i], null = 0, psd = psd)
+##             }
+##         })
+##         return(bf)
 ##     })
-##     se <- sapply(seq_along(n), FUN = function(i) {
-##         sqrt(2/n[i])
-##     })
-##     bf <- sapply(seq_along(n), FUN = function(i) {
-##         if (type == "normal") {
-##             bf01(estimate = smd[i], se = se[i], null = 0, pm = pm, psd = psd)
-##         } else if (type == "directional") {
-##             dirbf01(estimate = smd[i], se = se[i], null = 0, pm = pm, psd = psd)
-##         } else {
-##             nmbf01(estimate = smd[i], se = se[i], null = 0, psd = psd)
-##         }
-##     })
-##     result <- "inconclusive"
-##     for (i in seq_along(bf)) {
-##         if (bf[i] >= k0) {
-##             result <- "H0"
-##             break
-##         }
-##         if (bf[i] <= k1) {
-##         ## if ((bf[i] < k1) & t[i] > 0) {
-##             result <- "H1"
-##             break
-##         }
-##     }
-##     result
-## })
 
-## pbf01seq(k1 = k1, k0 = k0, se = se, n = n, pm = pm, psd = psd, dpm = dpm,
-##          dpsd = dpsd, type = type, strict = TRUE)
-## mean(results == "H1")
-## mean(results == "H0")
-## mean(results == "inconclusive")
+##     ## estimate probabilities
+##     stop <- apply(X = bfmat, MARGIN = 2, FUN = function(x) {
+##         result <- "inconclusive"
+##         for (xi in x) {
+##             if (xi >= k0) {
+##                 result <- "H0"
+##                 break
+##             }
+##             if (xi <= k1) {
+##                 result <- "H1"
+##                 break
+##             }
+##         }
+##         return(result)
+##     })
+##     pH1sim <- mean(stop == "H1")
+##     pH0sim <- mean(stop == "H0")
+##     pIncsim <- mean(stop == "inconclusive")
+
+##     ## compute probabilities numerically
+##     se <- sqrt(2/n)
+##     res <- bfpwr::pbf01seq(k1 = k1, k0 = k0, se = se, n = n, pm = pm,
+##                            psd = psd, dpm = dpm, dpsd = dpsd, type = type,
+##                            strict = TRUE)
+
+##     ## put everything together
+##     out <- data.frame(method = c("simulation", "numerical"),
+##                       pH1 = c(pH1sim, tail(res$cumpH1, n = 1)),
+##                       pH0 = c(pH0sim, tail(res$cumpH0, n = 1)),
+##                       pInc = c(pIncsim, tail(res$cumpInc, n = 1)))
+##     return(out)
+## }
+
+## simgrid <- expand.grid(k1 = c(1/10, 0.3),
+##                        k0 = c(3, 10),
+##                        usd = c(sqrt(2)),
+##                        pm = c(-0.25, 0.5),
+##                        dpm = c(0, 0.5),
+##                        psd = c(0.1, 1),
+##                        dpsd = c(0, 0.1),
+##                        type = c("normal", "moment", "directional"),
+##                        stringsAsFactors = FALSE)
+## n <- seq(25, 250, 25)
+## set.seed(42)
+## nsim <- 10000
+## simres <- do.call("rbind", lapply(X = seq(1, nrow(simgrid)), FUN = function(i) {
+##     res <- simbenchmark(nsim = nsim, k1 = simgrid$k1[i], k0 = simgrid$k0[i],
+##                         usd = simgrid$usd[i], n = n, pm = simgrid$pm[i],
+##                         psd = simgrid$psd[i], dpm = simgrid$dpm[i],
+##                         dpsd = simgrid$dpsd[i], type = simgrid$type[i])
+##     condition <- simgrid[i,]
+##     data.frame(i = i, condition, res)
+## }))
+
+## for (i in seq(1, nrow(simgrid))) {
+##     pH1sim <- simres[simres$i == i,]$pH1[1]
+##     pH1num <- simres[simres$i == i,]$pH1[2]
+##     print(expect_equal(pH1sim, pH1num, info = i, tolerance = 0.01))
+## }
+
+## library(ggplot2)
+## simplt <- ggplot(data = simres, aes(x = method, y = pH0, color = method,
+##                                     group = interaction(pm, dpm, psd, type, k1,
+##                                                         k0, dpsd))) +
+##     facet_grid(pm + dpm + psd + type ~ k1 + k0 + dpsd,
+##                labeller = label_both) +
+##     geom_line(color = 1, alpha = 0.8) +
+##     geom_point(size = 1) +
+##     theme_bw()
+## ggsave(plot = simplt, filename = "simbenchmark.pdf", width = 20, height = 20)
